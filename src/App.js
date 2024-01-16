b2d274b9fff1e57fffe08863d63a82f63838fa5e
6ZachDavid9 <reyianntigley1@gmail.com>
2024-01-16 20:21:21 +0800

    putang ina

--- src/App.js
+++ src/App.js
@@ -9,16 +9,12 @@ function App() {
 
   return (
     <div className="App">
-      <>
-      <Router>
         <Navbar />
         <Routes>
         <Route path='/' element={ <Home />}></Route>
         <Route path='/destinations' element={ <Destinations />}></Route>
         <Route path='/feedback' element={ <Feedback />}></Route>
         </Routes>
-      </Router>
-      </>
     </div>
   );
 }
