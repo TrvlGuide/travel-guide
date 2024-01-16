33bd3be1b3bd6643774c8a31daa8d71fefd5350a
6ZachDavid9 <reyianntigley1@gmail.com>
2024-01-16 20:18:28 +0800

    putang ina

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Home from './components/pages/Home';
 import Destinations from './components/pages/Destinations';
 import Feedback from './components/pages/Feedback';
 function App() {
+
+
   return (
     <div className="App">
+      <>
+      <Router>
         <Navbar />
         <Routes>
         <Route path='/' element={ <Home />}></Route>
         <Route path='/destinations' element={ <Destinations />}></Route>
         <Route path='/feedback' element={ <Feedback />}></Route>
         </Routes>
+      </Router>
+      </>
     </div>
   );
 }
