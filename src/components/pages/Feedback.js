cb823836d79dfb7565c8ae6678fe8bdec9455173
6ZachDavid9 <reyianntigley1@gmail.com>
2024-01-16 20:32:07 +0800

    putang ina

--- src/components/pages/Feedback.js
+++ src/components/pages/Feedback.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Feedback() {
   return (
     <div>Feedback1
-<img className='home-background' src='https://github.com/TrvlGuide/travel-guide/blob/5613784b988849fa0f42de4b5b85bc5b4764c3e0/public/images/home-background.jpg' alt="" />
+<img className='home-background' src='../images/home-background.jpg' alt="" />
 
     </div>
     
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
 
   return (
     <div className={`home-container ${isLoaded ? 'loaded' : ''}`}>
-      <img className='home-background' src="https://github.com/TrvlGuide/travel-guide/blob/5613784b988849fa0f42de4b5b85bc5b4764c3e0/public/images/home-background.jpg" alt="" />
+      <img className='home-background' src="../images/home-background.jpg" alt="" />
       <div className='title'>
         <h2>Travel Guide</h2>
         <p>Exploring Various Eco-Friendly Destinations in Cavite</p>
